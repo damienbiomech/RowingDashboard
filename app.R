0e83192e893ad1bc7e02e60843a144ed37742afe
damienbiomech <57115714+damienbiomech@users.noreply.github.com>
2026-07-02 09:35:51 +1000

    [ludis] Update file app.R

--- app.R
+++ app.R
@@ -28,6 +28,7 @@ options(shiny.port = 80)
 headers <- c('X-api-key' = Sys.getenv('LUDIS_API_TOKEN'))
 LUDIS_KEY <- Sys.getenv("LUDIS_API_TOKEN")
 dataset_id = "90c3600"
+print(LUDIS_KEY)
 
 ###############################################################################
 

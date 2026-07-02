910a4d887ebacc56baa4ef608d4482cb6257bfb8
damienbiomech <57115714+damienbiomech@users.noreply.github.com>
2026-07-02 11:58:37 +1000

    [ludis] Update file app_test.R
    Proxy commit by Github user sunjaipatil

---
+++ app_test.R
@@ -0,0 +1,36 @@
+library(shiny)
+library(tidyverse)
+library(data.table)
+library(plotly)
+library(shinyWidgets)
+library(shinydashboard)
+library(DBI)
+library(smartabaseR)
+library(httr2)
+library(readr)
+
+################################################################################
+
+# Helpers #
+
+cDir <- '/srv/shiny-server'
+
+shiny::addResourcePath('www', '/srv/shiny-server/www')
+
+options(shiny.host = '0.0.0.0')
+options(shiny.port = 80)
+
+headers <- c('X-api-key' = Sys.getenv('LUDIS_API_TOKEN'))
+LUDIS_KEY <- Sys.getenv("LUDIS_API_TOKEN")
+dataset_id = "90c3600"
+print(LUDIS_KEY)
+
+##############################################################################
+
+## Import Local Data Files ###
+
+# Load Athlete & Club list ##
+filename = 'Athlete_list.csv'
+url_file  <- sprintf(
+  "https://prod-backend.ludisanalytics.com/v2/api/ludisurl/%s?filePath=%s", dataset_id, filename)
+print(url_file)
